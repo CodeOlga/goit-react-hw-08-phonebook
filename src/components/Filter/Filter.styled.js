6f0ad20b052395ee0ad940825b2860abdc619e2c
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-09-01 11:01:52 +0300

    update css

--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -11,9 +11,12 @@ export const FilterInput = styled.input`
   border-radius: 20px;
   font-size: 16px;
   text-align: end;
-  border: 1px solid #00adb5;
-  // border: 0;
+  // border: 1px solid #00adb5;
+  border: 0;
   outline: 0;
   background-color: #e9e9e9;
-  // box-shadow: -5px -7px 9px #fff inset, 5px 11px 17px -13px #000 inset;
+  -webkit-box-shadow: -5px -7px 9px #fff inset, 5px 11px 17px -13px #000 inset;
+  // box-shadow:
+  // -5px -7px 9px #fff, 5px 11px 17px -13px #000, -5px -7px 9px #fff;
+  // 5px 11px 17px -13px #000;
 `;
